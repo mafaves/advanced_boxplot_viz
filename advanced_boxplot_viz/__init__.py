1928601d08d5ca1aacf0db01287bbcc3a8978fc6
Marcos Aguilella <94398282+mafaves@users.noreply.github.com>
2025-02-11 10:21:05 +0100

    Create __init__.py

---
+++ advanced_boxplot_viz/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 11/2/25 10:20 AM
+# @Author  : Marcos Aguilella
+# @Affiliation  : IDIVAL
+# @Email   : marcos.aguilella@idival.org
+# @File    : __init__.py
+
+from .boxplot_with_significance_bars import *
