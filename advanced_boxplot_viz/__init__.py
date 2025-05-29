0e447bed136590a01c0ee7299ecc4ac8eea46e12
Marcos Aguilella <marcos.aguilella.3n@gmail.com>
2025-05-29 09:33:26 +0200

    Add test_group_differences function to stats module imports

--- advanced_boxplot_viz/__init__.py
+++ advanced_boxplot_viz/__init__.py
@@ -7,8 +7,8 @@
 
 from .boxplot_with_significance_bars import generate_boxplots_with_significance
 from .utils import collect_p_values
-from .stats import check_variance_homogeneity, check_normality
+from .stats import check_variance_homogeneity, check_normality, test_group_differences
 
 __all__ = ['generate_boxplots_with_significance', 'collect_p_values', 'check_variance_homogeneity', 'check_normality']
-
+__version__ = "1.0.0"
 
